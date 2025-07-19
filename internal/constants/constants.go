8fc971b69d43755b8b92a1724fd815b759f458ed
Vítor Oliveira. <vitor.oalmeida@mercadolivre.com>
2025-07-19 00:08:36 -0300

    feat: add SpotifyUserFavoriteTracksFile constant for improved file path management

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -3,8 +3,9 @@ package constants
 const (
 
 	// File paths
-	DeezerTracksFile  = "deezer/track_info.json"
-	SpotifyTracksFile = "spotify/track_uri.json"
+	DeezerTracksFile              = "deezer/track_info.json"
+	SpotifyTracksFile             = "spotify/track_uri.json"
+	SpotifyUserFavoriteTracksFile = "spotify/favorite_tracks.json"
 
 	SpotifyPlaylistEndpoint = "https://api.spotify.com/v1/me/playlists"
 )
